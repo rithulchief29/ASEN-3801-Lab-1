06c6f720886ad3b61939c734889e43baed92ec02
AlatarRhys <47180874+AlatarRhys@users.noreply.github.com>
2026-01-27 13:33:31 -0700

    Add files via upload

--- Lab_2.m
+++ Lab_2.m
@@ -1,28 +1,29 @@
-clear; close all; clc;
+clc
+clear all
+close all
 
-data_1 = readmatrix("3801_Sec002_Test1.csv");
-data_2 = readmatrix("3801_Sec002_Test2.csv");
-data_3 = readmatrix("3801_Sec002_Test3.csv");
+filename ='3801_Sec002_Test3_Cleaned.csv';
+
+[t_vec, av_pos_inert, av_att, tar_pos_inert, tar_att] = LoadASPENData(filename);
+
 
 %% Questions 3
 
 %Data set 3
 figure();
-plot3(data_3(:,11), data_3(:,12), data_3(:,13),'color', 'blue',LineWidth = 1);
+plot3(av_pos_inert(:,1), av_pos_inert(:,2), av_pos_inert(:,3),'color', 'blue',LineWidth = 1);
 hold on
-plot3(data_3(:,5), data_3(:,6), data_3(:,7), '--r',LineWidth = 1);
+plot3(tar_pos_inert(:,1), tar_pos_inert(:,2), tar_pos_inert(:,3), '--r',LineWidth = 1);
 hold off
 
-xlabel('X-axis (mm)');
-ylabel('Y-axis (mm)');
-zlabel('Z-axis (mm)');
-title('AV Position vs Target Position from Data 3');
+xlabel('X-axis (m)');
+ylabel('Y-axis (m)');
+zlabel('Z-axis (m)');
+title('AV Position vs Target Position from Test 3');
 legend("A/V Path", "Target Path")
 grid on;
 
 %% Question 4
-
-time_3 = data_3(:,1)./100;
 
 %Figure 1
 
@@ -30,33 +31,33 @@ time_3 = data_3(:,1)./100;
 figure();
 
 subplot(3,1,1);
-plot(time_3, data_3(:,5), 'b', LineWidth=1);
+plot(t_vec, av_pos_inert(1,:), 'b', LineWidth=1);
 hold on
-plot(time_3, data_3(:,11), '--r', LineWidth=1);
+plot(t_vec, tar_pos_inert(1,:), '--r', LineWidth=1);
 hold off
 xlabel('Time (s)');
-ylabel('X Position (mm)');
+ylabel('X Position (m)');
 title('Position X vs Time for Data Set 3');
 legend("A/V Position", "Target Position");
 
 subplot(3,1,2);
-plot(time_3, data_3(:,6), 'b', LineWidth=1);
+plot(t_vec, av_pos_inert(2,:), 'b', LineWidth=1);
 hold on
-plot(time_3, data_3(:,11), '--r', LineWidth=1);
+plot(t_vec, tar_pos_inert(2,:), '--r', LineWidth=1);
 hold off
 xlabel('Time (s)');
-ylabel('Y Position (mm)');
-title('Position Y vs Time for Data Set 3');
+ylabel('Y Position (m)');
+title('Position Y vs Time for Test 3');
 legend("A/V Position", "Target Position");
 
 subplot(3,1,3);
-plot(time_3, data_3(:,7), 'b', LineWidth=1);
+plot(t_vec, av_pos_inert(3,:), 'b', LineWidth=1);
 hold on
-plot(time_3, data_3(:,13), '--r', LineWidth=1);
+plot(t_vec, tar_pos_inert(3,:), '--r', LineWidth=1);
 hold off
 xlabel('Time (s)');
-ylabel('Z Position (mm)');
-title('Position Z vs Time for Data Set 3');
+ylabel('Z Position (m)');
+title('Position Z vs Time for Test 3');
 legend("A/V Position", "Target Position");
 
 %figure 2
@@ -64,33 +65,33 @@ legend("A/V Position", "Target Position");
 figure();
 
 subplot(3,1,1);
-plot(time_3, (data_3(:,2).*(180/pi)), 'b', LineWidth=1);
+plot(t_vec, (av_att(1,:).*(180/pi)), 'b', LineWidth=1);
 hold on
-plot(time_3, (data_3(:,7).*(180/pi)), '--r', LineWidth=1);
+plot(t_vec, (tar_att(1,:).*(180/pi)), '--r', LineWidth=1);
 hold off
 xlabel('Time (s)');
 ylabel('Phi (degree)');
-title('Phi vs Time for Data Set 3');
+title('Phi vs Time for Test 3');
 legend("A/V Angle", "Target Angle");
 
 subplot(3,1,2);
-plot(time_3, (data_3(:,3).*(180/pi)), 'b', LineWidth=1);
+plot(t_vec, (av_att(2,:).*(180/pi)), 'b', LineWidth=1);
 hold on
-plot(time_3, (data_3(:,8).*(180/pi)), '--r', LineWidth=1);
+plot(t_vec, (tar_att(2,:).*(180/pi)), '--r', LineWidth=1);
 hold off
 xlabel('Time (s)');
 ylabel('Theta (degree)');
-title('Theta vs Time for Data Set 3');
+title('Theta vs Time for Test 3');
 legend("A/V Angle", "Target Angle");
 
 subplot(3,1,3);
-plot(time_3, (data_3(:,4).*(180/pi)), 'b', LineWidth=1);
+plot(t_vec, (av_att(3,:).*(180/pi)), 'b', LineWidth=1);
 hold on
-plot(time_3, (data_3(:,9).*(180/pi)), '--r', LineWidth=1);
+plot(t_vec, (tar_att(3,:).*(180/pi)), '--r', LineWidth=1);
 hold off
 xlabel('Time (s)');
 ylabel('Psi (degree)');
-title('Psi vs Time for Data Set 3');
+title('Psi vs Time for Test 3');
 legend("A/V Angle", "Target Angle");
 
 %% Question 5
